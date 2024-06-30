29045a02a842c9289568012fc028348b29592d75
xfe18827675401 <xfe18827675401@126.com>
2024-06-30 23:07:33 +0800

    添加项目文件。

---
+++ FileSystemWatcher/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Controls;
+
+namespace FileSystemWatcher.ViewModel;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    public MainWindow ViewPage { get; set; }
+
+    [ObservableProperty]
+    private Page? currentPage;
+
+
+    public MainWindowViewModel(MainWindow viewPage)
+    {
+        ViewPage = viewPage;
+    }
+
+    public MainWindowViewModel()
+    {
+    }
+}
