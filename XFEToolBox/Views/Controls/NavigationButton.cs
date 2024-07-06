91e6d3a669b46d8c0d330c317639ae7da7254d0b
xfe18827675401 <xfe18827675401@126.com>
2024-07-06 12:30:33 +0800

    设计按钮

---
+++ XFEToolBox/Views/Controls/NavigationButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace XFEToolBox.Views.Controls;
+
+public class NavigationButton : RadioButton
+{
+    static NavigationButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(NavigationButton), new PropertyMetadata(typeof(NavigationButton)));
+    }
+}
