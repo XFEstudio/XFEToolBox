ea7a9359893a849a81f89fca74596fa3919fece8
XFEstudio <mail@xfegzs.com>
2026-07-24 13:29:44 +0800

    添加主要配置文件

--- XFEToolBox.Server/Profiles/Data/MainDataProfile.cs
+++ XFEToolBox.Server/Profiles/Data/MainDataProfile.cs
@@ -4,5 +4,8 @@ namespace XFEToolBox.Server.Profiles.Data;
 
 public partial class MainDataProfile : XFEProfile
 {
-
+    [ProfileProperty]
+    [ProfilePropertyAddGet("Current.downloadAddressList.CurrentProfile = Current")]
+    [ProfilePropertyAddGet("return Current.downloadAddressList")]
+    private ProfileList<string> downloadAddressList = [];
 }
